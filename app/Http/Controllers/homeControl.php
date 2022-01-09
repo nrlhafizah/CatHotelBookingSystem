90af249113e0a39d64c8639180091e784fd27806
Nurul Hafizah <nrlhafizahkmrddn@gmail.com>
2022-01-09 15:00:34 +0800

    Template has been decided

---
+++ app/Http/Controllers/homeControl.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class homeControl extends Controller
+{
+    //
+
+    function index()
+    {
+        return view("home");
+    }
+}
